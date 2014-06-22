ac2c771b6761535de7de6f2df587bb18fdbe8fda
Txinto Vaz <txinto@gmail.com>
2014-06-23 00:53:25 +0100

    minor format changes

--- gatArduinOSEKsample/gatArduinOSEKsample.ino
+++ gatArduinOSEKsample/gatArduinOSEKsample.ino
@@ -11,8 +11,8 @@
  -- The serial console commands are processed in prj_tty_cmds.h and prj_tty_cmds.c
  -- The serial console broadcast of the input statuses is done in prj_tty_brcast.h and prj_tty_brcast.c
  -- Please visit the wiki page for this project in http://gatatac.org/projects/gttcosarduino/wiki/GatArduinOSEKino_example_1
-
-
+ 
+ 
 /***** gatArduinOSEK includes *****/
 /*** Types ***/
 #include <gttc_types.h>
@@ -52,37 +52,38 @@ void setup() {
 
 void loop()
 {
-  
+
   // ----------- Functionality ----------------
-  
+
   prjInput();
 
   // Button statuses broadcast through serial console
   buttonBroadcast();
 
- // Serial console commands handling (see prj_tty_cmds.h for protocol and functionality)
+  // Serial console commands handling (see prj_tty_cmds.h for protocol and functionality)
   // We will try to read and parse 3 times on each loop, so as to allow at least 3 characters (a command) if they come separately
   ttyCmdHandle(3);
-  
+
   prjOutput();
-  
+
   // ----------- End of Cycle Synchronization ----------------
-  
+
   // Now the microcontroller will loose time until the end of cycle sincronization time expires
   boolean timSync=timerSync();
   while(timSync==false){
     if ((CYCLE_TIME_MICROS-busyMicros)>CYCLE_SECURITY_TIME_MICROS){
-      
-      
+
+
       /* As the remaining time is bigger than a secure quantity, we will try to perform a serial console parsing action, in order to reduce the answering time. */
       /* This security time must be set depending on the project (and depending of the worst case expected time for this command parsing action).  The quantity is part of the project
-         configuration, not part of the OSEK configuration.  In this case it is part of the prj_tty_cmd.h file */
+       configuration, not part of the OSEK configuration.  In this case it is part of the prj_tty_cmd.h file */
       ttyCmdHandle(1);      
     }
     // timerSync returns true when the end of cycle syncronization time expired.
     timSync=timerSync();
   }
 }
+
 
 
 
