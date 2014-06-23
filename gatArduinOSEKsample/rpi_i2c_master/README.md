cf6d2c0d77c6cf4ec716fa7b95318de4c92af2cd
Txinto Vaz <txinto@gmail.com>
2014-06-23 20:26:23 +0000

    Adding I2C master software for testing

---
+++ gatArduinOSEKsample/rpi_i2c_master/README.md
@@ -0,0 +1,4 @@
+This folder contains software to program a I2C master in a Raspberry Pi.
+It is used to test & validate the Arduino I2C handler software.
+
+
---
+++ gatArduinOSEKsample/rpi_i2c_master/i2cTest.c
@@ -0,0 +1,83 @@
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+// The PiWeather board i2c address
+#define ADDRESS 0x04
+
+//#define DEBUG_I2C_LOAD
+
+#ifdef DEBUG_I2C_LOAD
+	#define I2C_WAIT 1000
+#else
+	#define I2C_WAIT 1000
+#endif
+
+// The I2C bus: This is for V2 pi's. For V1 Model B you need i2c-0
+static const char *devName = "/dev/i2c-1";
+
+int main(int argc, char** argv) {
+
+  if (argc == 1) {
+    printf("Supply one or more commands to send to the Arduino\n");
+    exit(1);
+  }
+
+  printf("I2C: Connecting\n");
+  int file;
+
+  if ((file = open(devName, O_RDWR)) < 0) {
+    fprintf(stderr, "I2C: Failed to access %d\n", devName);
+    exit(1);
+  }
+
+  printf("I2C: acquiring buss to 0x%x\n", ADDRESS);
+
+  if (ioctl(file, I2C_SLAVE, ADDRESS) < 0) {
+    fprintf(stderr, "I2C: Failed to acquire bus access/talk to slave 0x%x\n", ADDRESS);
+    exit(1);
+  }
+
+  int arg;
+
+  for (arg = 1; arg < argc; 
+#ifndef DEBUG_I2C_LOAD
+arg++
+#endif
+) {
+    char val[4];
+    unsigned char cmd[16];
+
+    if (0 == sscanf(argv[arg], "%s", val)) {
+      fprintf(stderr, "Invalid parameter %d \"%s\"\n", arg, argv[arg]);
+      exit(1);
+    }
+#ifndef DEBUG_I2C_LOAD
+    printf("Sending %s\n", val);
+#endif
+    if (write(file, val, 3) == 3) {
+#ifndef DEBUG_I2C_LOAD
+      printf("Argument sent!\n");
+#endif
+    }
+    usleep(I2C_WAIT);
+      char buf[]="01234567";
+      if (read(file, buf, 8) == 8) {
+#ifndef DEBUG_I2C_LOAD
+    printf("Received %s    \n", buf);
+#endif
+}
+
+    // Now wait else you could crash the arduino by sending requests too fast
+    usleep(I2C_WAIT);
+  }
+
+  close(file);
+  return (EXIT_SUCCESS);
+}
